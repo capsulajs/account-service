4a953c022360e7a376ceeceb50190057c4787b1d
Taras Shedenko <taras.shedenko@jivygroup.com>
2018-09-13 16:29:34 +0300

    WebSocket POC

--- integration/AccountService.test.js
+++ integration/AccountService.test.js
@@ -1,10 +1,29 @@
 // @flow
 
-import { AccountService } from 'AccountService';
-import { AxiosDispatcher } from 'transport/HTTP/AxiosDispatcher';
+const WebSocket = require('ws');
 
-const accountService = new AccountService(
-  new AxiosDispatcher({
-    baseUrl: ''
-  })
-);
+let request = require('./AccountService.data.json');
+request.d.token = require('./Auth0_security_token.json');
+
+console.log(/*'TOKEN: ', */request);
+
+const wsUrl = 'wss://configuration-service-7070.genesis.om2.com/';
+
+describe('A miserable attempt to establish a WebSocket connection', () => {
+  it('Test WebSocket', () => {
+    const ws = new WebSocket(wsUrl);
+
+    ws.on('open', () => {
+      console.log('OPEN');
+      ws.send(JSON.stringify(request));
+    });
+
+    ws.on('message', message => {
+      console.log('MESSAGE: ', JSON.parse(message));
+      ws.close();
+    });
+
+    ws.on('error', error => console.log('ERROR: ', error));
+    ws.onclose = ({ code, reason }) => console.log(`CLOSE: ${code}: ${reason}`);
+  });
+});
