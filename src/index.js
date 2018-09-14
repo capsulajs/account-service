c1935e2e6573376bbb29dc1ec48955e351f85570
Taras Shedenko <taras.shedenko@jivygroup.com>
2018-09-14 18:29:44 +0300

    Optimize file directory structure

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { AccountService } from './Provider/AccountService';
+export { AxiosDispatcher } from './transport/HTTP/AxiosDispatcher/AxiosDispatcher';
+export { WebSocketDispatcher } from './transform/WebSocket/WebSocketDispatcher';
